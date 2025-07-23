Kuadrant/osia

commit a023c78d90edbb38f5bf0060b0f91bf119291c3f

Fix mypy errors in installer/downloader/install.py

osia/installer/downloader/install.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 """Module responsible for download of openshift-install binary"""
 from shutil import copyfileobj
-from tempfile import NamedTemporaryFile
+from tempfile import _TemporaryFileWrapper
 from pathlib import Path
 from typing import Tuple, Optional
 import platform
@@ -55,7 +55,7 @@ def _current_platform() -> Tuple[str, str]:
 
 
 def get_url(directory: str, arch: str, fips: bool = False,
-            rhel_version: str = None) -> Tuple[Optional[str], Optional[str]]:
+            rhel_version: str | None = None) -> Tuple[str, Optional[str]]:
     """Searches the http directory and returns both url to installer
     and version.
     """
@@ -93,7 +93,7 @@ def get_url(directory: str, arch: str, fips: bool = False,
 
 
 def get_devel_url(version: str, arch: str, fips: bool = False,
-                  rhel_version: str = None) -> Tuple[Optional[str], Optional[str]]:
+                  rhel_version: str | None = None) -> Tuple[str, Optional[str]]:
     """
     Searches developement sources and returns url to installer
     """
@@ -110,13 +110,13 @@ def get_devel_url(version: str, arch: str, fips: bool = False,
 
 
 def get_prev_url(version: str, arch: str, fips: bool = False,
-                 rhel_version: str = None) -> Tuple[Optional[str], Optional[str]]:
+                 rhel_version: str | None = None) -> Tuple[str, Optional[str]]:
     """Returns installer url from dev-preview sources"""
     return get_url(PREVIEW_ROOT.format(arch) + version + "/", arch, fips, rhel_version)
 
 
 def get_prod_url(version: str, arch: str, fips: bool = False,
-                 rhel_version: str = None) -> Tuple[Optional[str], Optional[str]]:
+                 rhel_version: str | None = None) -> Tuple[str, Optional[str]]:
     """Returns installer url from production sources"""
     return get_url(PROD_ROOT.format(arch) + version + "/", arch, fips, rhel_version)
 
@@ -129,7 +129,7 @@ def _get_storage_path(version: str, install_base: str) -> str:
     return spec_path.as_posix()
 
 
-def _extract_tar(buffer: NamedTemporaryFile, target: str) -> Path:
+def _extract_tar(buffer: _TemporaryFileWrapper[bytes], target: str) -> Path:
     result = None
     with tarfile.open(buffer.name) as tar:
         inst_info = None
@@ -139,6 +139,8 @@ def _extract_tar(buffer: NamedTemporaryFile, target: str) -> Path:
         if inst_info is None:
             raise Exception("error")
         stream = tar.extractfile(inst_info)
+        if stream is None:
+            raise IOError("could not extract tar")
         result = Path(target).joinpath(inst_info.name)
         with result.open('wb') as output:
             copyfileobj(stream, output)
@@ -146,7 +148,7 @@ def _extract_tar(buffer: NamedTemporaryFile, target: str) -> Path:
     return result
 
 
-def get_installer(tar_url: str, target: str):
+def get_installer(tar_url: str, target: str) -> str:
     """Download and extract the installer into the target"""
     return get_data(tar_url, target, _extract_tar)
 
@@ -157,7 +159,7 @@ def download_installer(installer_version: str,
                        dest_directory: str,
                        source: str,
                        fips: bool = False,
-                       rhel_version: str = None) -> str:
+                       rhel_version: str | None = None) -> str:
     """Starts search and extraction of installer"""
     logging.debug("Getting version %s of %s, storing to directory %s and devel is %r",
                   installer_version, installer_arch, dest_directory, source)
@@ -174,7 +176,9 @@ def download_installer(installer_version: str,
 
     url, version = downloader(installer_version, installer_arch, fips, rhel_version)
     logging.debug('Installer\'s URL is  %s and full version is %s', url, version)
-    root = Path(dest_directory).joinpath(version)
+    root = Path(dest_directory)
+    if version:
+        root = root.joinpath(version)
 
     installer_exe_name = 'openshift-install'
 

pyproject.toml
@@ -59,5 +59,4 @@ build-backend = "poetry.core.masonry.api"
 exclude = [
         'osia/cli.py',
         'osia/config/config.py',
-        'osia/installer/downloader/install.py',
 ]

commit 999a53aa254f27daf740f5351759babfb860e990

deffer evaluation of annotations for _TemporaryFileWrapper type
Use _PEP 649 – Deferred Evaluation Of Annotations Using Descriptors_ in
files where _TemporaryFileWrapper is used

execution of `osia install` results in:
  Traceback: `TypeError: type '_TemporaryFileWrapper' is not subscriptable`

Fixes: a023c78 ("Fix mypy errors in installer/downloader/install.py")
Fixes: dcb6c68 ("Fix mypy errors in installer/downloader/image.py")
Fixes: 4535238 ("Fix mypy error in installer/downloader/utils.py")

osia/installer/downloader/image.py
@@ -1,4 +1,6 @@
 """Module implements logic for rhcos image download"""
+from __future__ import annotations
+
 import gzip
 import json
 import logging

osia/installer/downloader/install.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Module responsible for download of openshift-install binary"""
+from __future__ import annotations
+
 import logging
 import platform
 import re

osia/installer/downloader/utils.py
@@ -1,5 +1,7 @@
 """Module implements utilitary functions shared by download
 package"""
+from __future__ import annotations
+
 import logging
 from collections.abc import Callable
 from pathlib import Path